539c133c9eef2c25d9c33a8e2cad946891da74bd
Luca Machado Mendonça <Lucammdev@gmail.com>
2024-04-10 23:47:11 -0300

    Home screen done

---
+++ lib/pages/home.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/login_background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * (2 / 3),
+            ),
+            Container(
+                color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          "Viagens com preços baixos para você",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xFF0E4B7C),
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )),
+                    Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.all(6),
+                        padding: const EdgeInsets.all(2),
+                        child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(
+                                width: 1.0,
+                                color: Color(0xFF00AFF8),
+                                style: BorderStyle.solid,
+                              ),
+                              backgroundColor: Color(0xFF00AFF8),
+                            ),
+                            onPressed: (() {
+                              Navigator.of(context).pushNamed(
+                                '/Login',
+                              );
+                            }),
+                            child: const Text(
+                              'Entrar',
+                              style: TextStyle(
+                                  color: Color(0xFFFAFAFA), fontSize: 24),
+                            ))),
+                    Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.all(6),
+                        padding: const EdgeInsets.all(2),
+                        child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(
+                                width: 1.0,
+                                color: Color(0xFFFAFAFA),
+                                style: BorderStyle.solid,
+                              ),
+                              backgroundColor: Color(0xFFFAFAFA),
+                            ),
+                            onPressed: (() {
+                              Navigator.of(context).pushNamed(
+                                '/cadastro',
+                              );
+                            }),
+                            child: const Text(
+                              'Cadastre-se',
+                              style: TextStyle(
+                                  color: Color(0xFF00AFF8), fontSize: 24),
+                            ))),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,3 +1,4 @@
+import 'package:app_uff_caronas/pages/home.dart';
 import 'package:app_uff_caronas/pages/login.dart';
 import 'package:app_uff_caronas/pages/cadastro.dart';
 import 'package:flutter/material.dart';
@@ -6,12 +7,14 @@ class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
     switch (settings.name) {
-      case '/':
+      case '/Home':
+        return MaterialPageRoute(builder: ((context) => const Home()));
+      case '/Login':
         return MaterialPageRoute(builder: ((context) => const Login()));
       case '/cadastro':
         return MaterialPageRoute(builder: ((context) => const Cadastro()));
       default:
-        return MaterialPageRoute(builder: ((context) => const Login()));
+        return MaterialPageRoute(builder: ((context) => const Home()));
     }
   }
 }
