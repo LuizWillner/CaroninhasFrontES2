4dd6486e553f6cc90f5a2f5b54fb39cb49412c47
Soljaa <danieljms@id.uff.br>
2024-05-26 20:34:15 -0300

    wip: pedir e ofercer carona novo layout

--- lib/pages/oferecer_carona.dart
+++ lib/pages/oferecer_carona.dart
@@ -10,31 +10,35 @@ class CriarCarona extends StatefulWidget {
   State<CriarCarona> createState() => _CriarCaronaState();
 }
 
-class _CriarCaronaState extends State<CriarCarona> {
+class _CriarCaronaState extends State<CriarCarona>
+    with SingleTickerProviderStateMixin {
   static const clearBlueColor = Color(0xFF00AFF8);
   static const darkBlueColor = Color(0xFF0E4B7C);
 
   TextEditingController _fromController = TextEditingController();
   TextEditingController _toController = TextEditingController();
-  TextEditingController _dateController = TextEditingController();
-  TextEditingController _timeController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+  TimeOfDay _selectedTime = TimeOfDay.now();
   TextEditingController _passagersController = TextEditingController();
   TextEditingController _priceController = TextEditingController();
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Oferecer carona',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          leading: const BackButton(color: Colors.white),
-          backgroundColor: clearBlueColor,
-        ),
         body: SingleChildScrollView(
           child: Container(
             color: Colors.white,
@@ -51,87 +55,142 @@ class _CriarCaronaState extends State<CriarCarona> {
                 ),
                 Column(
                   children: [
-                    Container(
-                      margin: const EdgeInsets.only(
-                          top: 120.0, left: 30.0, right: 30.0),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: clearBlueColor,
+                    const Padding(
+                      padding: EdgeInsets.only(top: 40.0),
+                      child: Center(
+                        child: Text(
+                          "Dê uma caroninha pra que ta precisando!", // TODO: mudar cor do fundo
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white),
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          FormInput(
-                            controller: _fromController,
-                            isObscured: false,
-                            placeholderText: "De (endereço completo)",
-                            fieldIcon: Icons.circle_outlined,
-                            keyboardType: TextInputType.text,
-                          ),
-                          FormInput(
-                            controller: _toController,
-                            isObscured: false,
-                            placeholderText: "Para (endereço completo)",
-                            fieldIcon: Icons.circle,
-                            keyboardType: TextInputType.text,
-                          ),
-                          FormInput(
-                            controller: _dateController,
-                            isObscured: false,
-                            placeholderText: "Hoje",
-                            fieldIcon: Icons.calendar_month,
-                            keyboardType: TextInputType.datetime,
-                          ),
-                          FormInput(
-                            controller: _timeController,
-                            isObscured: false,
-                            placeholderText: "Hora",
-                            fieldIcon: Icons.lock_clock,
-                            keyboardType: TextInputType.number,
-                          ),
-                          FormInput(
-                            controller: _passagersController,
-                            isObscured: false,
-                            fieldIcon: Icons.person,
-                            placeholderText: "Passageiros",
-                            keyboardType: TextInputType.text,
-                          ),
-                          FormInput(
-                            controller: _priceController,
-                            isObscured: false,
-                            placeholderText: "Preço",
-                            fieldIcon: Icons.price_change,
-                            keyboardType: TextInputType.text,
-                          ),
-                          const SizedBox(height: 16.0),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
-                            child: OutlinedButton(
-                              style: OutlinedButton.styleFrom(
-                                side: const BorderSide(
-                                  width: 1.0,
-                                  color: Color(0xFF00AFF8),
-                                  style: BorderStyle.solid,
-                                ),
-                                backgroundColor: clearBlueColor,
-                              ),
-                              onPressed: null,
-                              child: const Text(
-                                'Oferecer',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 24),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 18.0),
-                        ],
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    )
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.03,
+                            left: 30.0,
+                            right: 30.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: clearBlueColor,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: Colors.white),
+                        child: TabBar(
+                          controller: _tabController,
+                          tabs: [
+                            Tab(child: Text('Criar carona')),
+                            Tab(child: Text('Aceitar carona'))
+                          ],
+                        )),
+                    Container(
+                        height: 450,
+                        margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: clearBlueColor,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: Colors.white),
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: TabBarView(
+                          controller: _tabController,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                FormInput(
+                                  controller: _fromController,
+                                  isObscured: false,
+                                  placeholderText: "De (endereço completo)",
+                                  fieldIcon: Icons.circle_outlined,
+                                  keyboardType: TextInputType.text,
+                                ),
+                                FormInput(
+                                  controller: _toController,
+                                  isObscured: false,
+                                  placeholderText: "Para (endereço completo)",
+                                  fieldIcon: Icons.circle,
+                                  keyboardType: TextInputType.text,
+                                ),
+                                TextButton(
+                                    onPressed: () async {
+                                      final DateTime? dateTime =
+                                          await showDatePicker(
+                                              context: context,
+                                              firstDate: DateTime(2000),
+                                              lastDate: DateTime(3000));
+                                      if (dateTime != null) {
+                                        setState(() {
+                                          _selectedDate = dateTime;
+                                        });
+                                      }
+                                    },
+                                    child: Text(
+                                        "${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}")),
+                                TextButton(
+                                    onPressed: () async {
+                                      final TimeOfDay? timeOfDay =
+                                          await showTimePicker(
+                                              context: context,
+                                              initialTime: _selectedTime,
+                                              initialEntryMode: TimePickerEntryMode.dial);
+                                      if (timeOfDay != null) {
+                                        setState(() {
+                                          _selectedTime = timeOfDay;
+                                        });
+                                      }
+                                    },
+                                    child: Text(
+                                        "${_selectedTime.hour}:${_selectedTime.minute}")),
+                                FormInput(
+                                  controller: _passagersController,
+                                  isObscured: false,
+                                  fieldIcon: Icons.person,
+                                  placeholderText: "Passageiros",
+                                  keyboardType: TextInputType.text,
+                                ),
+                                FormInput(
+                                  controller: _priceController,
+                                  isObscured: false,
+                                  placeholderText: "Preço",
+                                  fieldIcon: Icons.price_change,
+                                  keyboardType: TextInputType.text,
+                                ),
+                                const SizedBox(height: 16.0),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(24, 0, 24, 0),
+                                  child: OutlinedButton(
+                                    style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                        width: 1.0,
+                                        color: Color(0xFF00AFF8),
+                                        style: BorderStyle.solid,
+                                      ),
+                                      backgroundColor: clearBlueColor,
+                                    ),
+                                    onPressed: null,
+                                    child: const Text(
+                                      'Oferecer',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 24),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 18.0),
+                              ],
+                            ),
+                            Text("lista de caronas")
+                          ],
+                        ))
                   ],
                 ),
               ],
--- lib/pages/pedir_carona.dart
+++ lib/pages/pedir_carona.dart
@@ -4,21 +4,36 @@ import 'package:app_uff_caronas/components/bottom_bar.dart';
 import 'package:app_uff_caronas/components/cadastro_input.dart';
 
 class PedirCarona extends StatefulWidget {
-  const PedirCarona({Key? key}) : super(key: key);
+  const PedirCarona({super.key});
 
   @override
   State<PedirCarona> createState() => _PedirCaronaState();
 }
 
-class _PedirCaronaState extends State<PedirCarona> {
+class _PedirCaronaState extends State<PedirCarona>
+    with SingleTickerProviderStateMixin {
   static const clearBlueColor = Color(0xFF00AFF8);
   static const darkBlueColor = Color(0xFF0E4B7C);
 
-  TextEditingController _fromController = TextEditingController();
-  TextEditingController _toController = TextEditingController();
-  TextEditingController _dateController = TextEditingController();
-  TextEditingController _timeController = TextEditingController();
-  TextEditingController _passagersController = TextEditingController();
+  final TextEditingController _fromController = TextEditingController();
+  final TextEditingController _toController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+  final TextEditingController _passagersController = TextEditingController();
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,96 +54,121 @@ class _PedirCaronaState extends State<PedirCarona> {
                 ),
                 Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 40.0),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 40.0),
                       child: Center(
                         child: Text(
-                          "Viagens com preços baixos para você",
+                          "Está procurando uma caroninha?",
                           style: TextStyle(
-                            fontSize: 30.0, 
-                            color: Colors.white, 
-                            fontWeight: FontWeight.bold, 
+                            fontSize: 30.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
-                          textAlign: TextAlign.center, 
+                          textAlign: TextAlign.center,
                         ),
                       ),
                     ),
                     Container(
-                      margin: const EdgeInsets.only(
-                          top: 120.0, left: 30.0, right: 30.0),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: clearBlueColor,
-                          ),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white),
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          FormInput(
-                            controller: _fromController,
-                            isObscured: false,
-                            placeholderText: "De (endereço completo)",
-                            fieldIcon: Icons.circle_outlined,
-                            keyboardType: TextInputType.text,
-                          ),
-                          FormInput(
-                            controller: _toController,
-                            isObscured: false,
-                            placeholderText: "Para (endereço completo)",
-                            fieldIcon: Icons.circle,
-                            keyboardType: TextInputType.text,
-                          ),
-                          FormInput(
-                            controller: _dateController,
-                            isObscured: false,
-                            placeholderText: "Hoje",
-                            fieldIcon: Icons.calendar_month,
-                            keyboardType: TextInputType.datetime,
-                          ),
-                          FormInput(
-                            controller: _timeController,
-                            isObscured: false,
-                            placeholderText: "Hora",
-                            fieldIcon: Icons.lock_clock,
-                            keyboardType: TextInputType.number,
-                          ),
-                          FormInput(
-                            controller: _passagersController,
-                            isObscured: false,
-                            fieldIcon: Icons.person,
-                            placeholderText: "Passageiros",
-                            keyboardType: TextInputType.text,
-                          ),
-                          const SizedBox(height: 16.0),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
-                            child: OutlinedButton(
-                              style: OutlinedButton.styleFrom(
-                                side: const BorderSide(
-                                  width: 1.0,
-                                  color: clearBlueColor,
-                                  style: BorderStyle.solid,
-                                ),
-                                backgroundColor: clearBlueColor,
-                              ),
-                              onPressed: () => {
-                                Navigator.of(context).pushNamed('/Pedindo_carona')
-                              },
-                              child: const Text(
-                                'Procurar',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 24),
-                              ),
+                        margin: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.03, left: 30.0, right: 30.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: clearBlueColor,
                             ),
-                          ),
-                          const SizedBox(height: 18.0),
-                        ],
-                      ),
-                    )
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: Colors.white),
+                        child: TabBar(
+                          controller: _tabController,
+                          tabs: [
+                            Tab(child: Text('Pedir carona')),
+                            Tab(child: Text('Encontrar carona'))
+                          ],
+                        )),
+                    Container(
+                      height: 400,
+                        margin: const EdgeInsets.only(
+                            left: 30.0, right: 30.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: clearBlueColor,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: Colors.white),
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: TabBarView(
+                          controller: _tabController,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                FormInput(
+                                  controller: _fromController,
+                                  isObscured: false,
+                                  placeholderText: "De (endereço completo)",
+                                  fieldIcon: Icons.circle_outlined,
+                                  keyboardType: TextInputType.text,
+                                ),
+                                FormInput(
+                                  controller: _toController,
+                                  isObscured: false,
+                                  placeholderText: "Para (endereço completo)",
+                                  fieldIcon: Icons.circle,
+                                  keyboardType: TextInputType.text,
+                                ),
+                                FormInput(
+                                  controller: _dateController,
+                                  isObscured: false,
+                                  placeholderText: "Data",
+                                  fieldIcon: Icons.calendar_month,
+                                  keyboardType: TextInputType.datetime,
+                                ),
+                                FormInput(
+                                  controller: _timeController,
+                                  isObscured: false,
+                                  placeholderText: "Hora",
+                                  fieldIcon: Icons.lock_clock,
+                                  keyboardType: TextInputType.number,
+                                ),
+                                FormInput(
+                                  controller: _passagersController,
+                                  isObscured: false,
+                                  fieldIcon: Icons.person,
+                                  placeholderText: "Passageiros",
+                                  keyboardType: TextInputType.text,
+                                ),
+                                const SizedBox(height: 16.0),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(24, 0, 24, 0),
+                                  child: OutlinedButton(
+                                    style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                        width: 1.0,
+                                        color: clearBlueColor,
+                                        style: BorderStyle.solid,
+                                      ),
+                                      backgroundColor: clearBlueColor,
+                                    ),
+                                    onPressed: () => {
+                                      Navigator.of(context)
+                                          .pushNamed('/Pedindo_carona')
+                                    },
+                                    child: const Text(
+                                      'Procurar',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 24),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 18.0),
+                              ],
+                            ),
+                            Text("lista de caronas")
+                          ],
+                        ))
                   ],
                 ),
               ],
