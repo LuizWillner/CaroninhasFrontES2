f5124b1798178491f7e69f675d520553df47cfe7
Luca Machado Mendonça <Lucammdev@gmail.com>
2024-06-15 15:21:55 -0300

    todas as caronas aparecem

--- lib/pages/pedir_carona.dart
+++ lib/pages/pedir_carona.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:app_uff_caronas/components/bottom_bar.dart';
 import 'package:app_uff_caronas/components/cadastro_input.dart';
@@ -58,78 +60,44 @@ class _PedirCaronaState extends State<PedirCarona>
     }
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Container(
-      color: Colors.white,
-      child: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * (5 / 10),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/login_background.png'),
-                fit: BoxFit.cover,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * (5 / 10),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/login_background.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 40.0),
-                child: Center(
-                  child: Text(
-                    "Está procurando uma caroninha?",
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.03,
-                  left: 30.0,
-                  right: 30.0,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: clearBlueColor,
-                  ),
-                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                  color: Colors.white,
-                ),
-                child: TabBar(
-                  controller: _tabController,
-                  indicator: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: const Color(0xFF00AFF8),
-                  ),
-                  labelColor: Colors.white,
-                  unselectedLabelColor: Colors.black,
-                  tabs: [
-                    Container(
-                      alignment: Alignment.center,
-                      child: const Tab(
-                        child: Text('Pedir carona'),
+            Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 40.0),
+                  child: Center(
+                    child: Text(
+                      "Está procurando uma caroninha?",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    Container(
-                      alignment: Alignment.center,
-                      child: const Tab(
-                        child: Text('Encontrar carona'),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.03,
+                    left: 30.0,
+                    right: 30.0,
+                  ),
                   decoration: BoxDecoration(
                     border: Border.all(
                       color: clearBlueColor,
@@ -137,102 +105,147 @@ Widget build(BuildContext context) {
                     borderRadius: const BorderRadius.all(Radius.circular(20)),
                     color: Colors.white,
                   ),
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: TabBarView(
+                  child: TabBar(
                     controller: _tabController,
-                    children: [
-                      SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            FormInput(
-                              controller: _fromController,
-                              isObscured: false,
-                              placeholderText: "De (endereço completo)",
-                              fieldIcon: Icons.circle_outlined,
-                              keyboardType: TextInputType.text,
-                            ),
-                            FormInput(
-                              controller: _toController,
-                              isObscured: false,
-                              placeholderText: "Para (endereço completo)",
-                              fieldIcon: Icons.circle,
-                              keyboardType: TextInputType.text,
-                            ),
-                            FormInput(
-                              controller: _dateController,
-                              isObscured: false,
-                              placeholderText: "Data",
-                              fieldIcon: Icons.calendar_month,
-                              keyboardType: TextInputType.datetime,
-                            ),
-                            FormInput(
-                              controller: _timeController,
-                              isObscured: false,
-                              placeholderText: "Hora",
-                              fieldIcon: Icons.lock_clock,
-                              keyboardType: TextInputType.number,
-                            ),
-                            FormInput(
-                              controller: _passagersController,
-                              isObscured: false,
-                              fieldIcon: Icons.person,
-                              placeholderText: "Passageiros",
-                              keyboardType: TextInputType.text,
-                            ),
-                            const SizedBox(height: 16.0),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
-                              child: OutlinedButton(
-                                style: OutlinedButton.styleFrom(
-                                  side: const BorderSide(
-                                    width: 1.0,
-                                    color: clearBlueColor,
-                                    style: BorderStyle.solid,
-                                  ),
-                                  backgroundColor: clearBlueColor,
-                                ),
-                                onPressed: () => _showMyDialog(context),
-                                child: const Text(
-                                  'Procurar',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 18.0),
-                          ],
+                    indicator: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: const Color(0xFF00AFF8),
+                    ),
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Colors.black,
+                    tabs: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Tab(
+                          child: Text('Pedir carona'),
                         ),
                       ),
-                      ListView.builder(
-                        itemCount: rides.length,
-                        itemBuilder: (context, index) {
-                          final ride = rides[index];
-                          return Viagem(
-                                        image: "assets/login_background.png",
-                                        partida: ride["local_partida"],
-                                        chegada: ride["local_destino"],
-                                        nome: ride["motorista"]["user"]["first_name"]+ride["motorista"]["user"]["last_name"],
-                                        data: DateFormat("yyyy-MM-ddTHH:mm:ss").parse(ride["hora_partida"]),
-                                        onPressed: () => {},
-                                        price: ride["valor"]) ; // Replace this with your actual widget to display ride information
-                        },
+                      Container(
+                        alignment: Alignment.center,
+                        child: const Tab(
+                          child: Text('Encontrar carona'),
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: clearBlueColor,
+                      ),
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white,
+                    ),
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: [
+                        SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              FormInput(
+                                controller: _fromController,
+                                isObscured: false,
+                                placeholderText: "De (endereço completo)",
+                                fieldIcon: Icons.circle_outlined,
+                                keyboardType: TextInputType.text,
+                              ),
+                              FormInput(
+                                controller: _toController,
+                                isObscured: false,
+                                placeholderText: "Para (endereço completo)",
+                                fieldIcon: Icons.circle,
+                                keyboardType: TextInputType.text,
+                              ),
+                              FormInput(
+                                controller: _dateController,
+                                isObscured: false,
+                                placeholderText: "Data",
+                                fieldIcon: Icons.calendar_month,
+                                keyboardType: TextInputType.datetime,
+                              ),
+                              FormInput(
+                                controller: _timeController,
+                                isObscured: false,
+                                placeholderText: "Hora",
+                                fieldIcon: Icons.lock_clock,
+                                keyboardType: TextInputType.number,
+                              ),
+                              FormInput(
+                                controller: _passagersController,
+                                isObscured: false,
+                                fieldIcon: Icons.person,
+                                placeholderText: "Passageiros",
+                                keyboardType: TextInputType.text,
+                              ),
+                              const SizedBox(height: 16.0),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(24, 0, 24, 0),
+                                child: OutlinedButton(
+                                  style: OutlinedButton.styleFrom(
+                                    side: const BorderSide(
+                                      width: 1.0,
+                                      color: clearBlueColor,
+                                      style: BorderStyle.solid,
+                                    ),
+                                    backgroundColor: clearBlueColor,
+                                  ),
+                                  onPressed: () async {
+                                    final response =
+                                        await authService.getRides();
+                                    setState(() {
+                                      rides = response["data"];
+                                    });
+                                    _showMyDialog(context);
+                                  },
+                                  child: const Text(
+                                    'Procurar',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 18.0),
+                            ],
+                          ),
+                        ),
+                        ListView.builder(
+                          itemCount: rides.length,
+                          itemBuilder: (context, index) {
+                            final ride = rides[index];
+                            return Viagem(
+                                image: "assets/login_background.png",
+                                partida: ride["local_partida"],
+                                chegada: ride["local_destino"],
+                                nome: ride["motorista"]["user"]["first_name"] +
+                                    ride["motorista"]["user"]["last_name"],
+                                data: DateFormat("yyyy-MM-ddTHH:mm:ss")
+                                    .parse(ride["hora_partida"]),
+                                onPressed: () => {},
+                                price: ride[
+                                    "valor"]); // Replace this with your actual widget to display ride information
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-    bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 0),
-  );
-}
+      bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 0),
+    );
+  }
 
   Future<void> _showMyDialog(BuildContext context) async {
     return showDialog<void>(
