ca8ec93ed1087f78e8b8d0b7059a5bdb6e62f445
Luca Machado Mendonça <Lucammdev@gmail.com>
2024-06-16 15:24:48 -0300

    histórico de caronas integrado

--- lib/pages/detalhes.dart
+++ lib/pages/detalhes.dart
@@ -4,6 +4,7 @@ import 'package:app_uff_caronas/components/path_details.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:app_uff_caronas/services/service_auth_and_user.dart';
 import 'package:app_uff_caronas/services/api_services.dart';
+import 'package:intl/intl.dart';
 
 class DetalhesCarona extends StatefulWidget {
   final String caronaId;
@@ -13,32 +14,31 @@ class DetalhesCarona extends StatefulWidget {
   State<DetalhesCarona> createState() => _DetalhesCaronaState();
 }
 
-
 class _DetalhesCaronaState extends State<DetalhesCarona> {
   static const clearBlueColor = Color(0xFF00AFF8);
   static const darkBlueColor = Color(0xFF0E4B7C);
-final AuthService authService = AuthService(apiService: ApiService());
-final storage = const FlutterSecureStorage();
-dynamic user;
-var rideDetail;
+  final AuthService authService = AuthService(apiService: ApiService());
+  final storage = const FlutterSecureStorage();
+  dynamic user;
+  var rideDetail;
 
-@override
-void initState() {
-  super.initState();
-  _fetchRide();
-}
-
-void _fetchRide() async {
-  try {
-    final ridesResponse = await authService.getRideById(widget.caronaId);
-
-    setState(() {
-      rideDetail = ridesResponse;
-    });
-  } catch (error) {
-    print(error.toString());
+  @override
+  void initState() {
+    super.initState();
+    _fetchRide();
   }
-}
+
+  void _fetchRide() async {
+    try {
+      final ridesResponse = await authService.getRideById(widget.caronaId);
+
+      setState(() {
+        rideDetail = ridesResponse;
+      });
+    } catch (error) {
+      print(error.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +72,7 @@ void _fetchRide() async {
                   ),
                   padding: const EdgeInsets.all(20.0),
                   child: Text(
-                      "Carona ID: ${widget.caronaId}"), // Displaying caronaId here
+                      "Foto do mapa"), // Displaying caronaId here
                 ),
                 Container(
                   width: 300.0,
@@ -91,7 +91,7 @@ void _fetchRide() async {
                               painter: AddressesPainter([
                                 rideDetail["local_partida"],
                                 "até",
-                                rideDetail["local_chegada"],
+                                rideDetail["local_destino"],
                               ]),
                             ),
                           ),
@@ -101,7 +101,7 @@ void _fetchRide() async {
                               children: [
                                 rideDetail["local_partida"],
                                 "até",
-                                rideDetail["local_chegada"],
+                                rideDetail["local_destino"],
                               ]
                                   .map((address) => Padding(
                                         padding:
@@ -126,11 +126,15 @@ void _fetchRide() async {
                           )
                         ],
                       ),
-                      const Text("4 de maio",
+                      Text(
+                          "${DateFormat("yyyy-MM-ddTHH:mm:ss").parse(rideDetail["hora_partida"]).day}/"
+                          "${DateFormat("yyyy-MM-ddTHH:mm:ss").parse(rideDetail["hora_partida"]).month}",
                           style: TextStyle(fontSize: 16.0, color: Colors.grey)),
-                      const Text("14:00",
+                      Text(
+                          "${DateFormat("yyyy-MM-ddTHH:mm:ss").parse(rideDetail["hora_partida"]).hour}:"
+                          "${DateFormat("yyyy-MM-ddTHH:mm:ss").parse(rideDetail["hora_partida"]).day}",
                           style: TextStyle(fontSize: 16.0, color: Colors.grey)),
-                      const Text("R\$ 22,90",
+                      Text("R\$ ${rideDetail["valor"]},00",
                           style: TextStyle(fontSize: 16.0, color: Colors.grey)),
                       const SizedBox(height: 18.0),
                       const Text("Tipo",
@@ -141,13 +145,15 @@ void _fetchRide() async {
                               color: darkBlueColor,
                               fontWeight: FontWeight.bold)),
                       const SizedBox(height: 18.0),
-                      const Text("Integrantes",
+                      const Text("passageiros",
                           style: TextStyle(fontSize: 16.0, color: Colors.grey)),
-                      const Text("Lista da tropa",
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              color: darkBlueColor,
-                              fontWeight: FontWeight.bold)),
+                      ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: rideDetail["passageiros"].length,
+                          itemBuilder: (context, index) {
+                            final caronista = "${rideDetail["passageiros"][index]["user"]["first_name"]} ${rideDetail["passageiros"][index]["user"]["first_name"]}";
+                            return Text(caronista);
+                          }),
                       const SizedBox(height: 18.0),
                     ],
                   ),
